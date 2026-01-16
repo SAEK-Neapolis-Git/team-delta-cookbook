fbc2d92fc357d8929dab6de322b108b97d4bbf76
efipara <efthimiapara06@gmail.com>
2026-01-16 18:06:15 +0200

    [+] Update to Recipe file

--- cookcbook/recipes/recipe_Efthimia_Paraschou.py
+++ cookcbook/recipes/recipe_Efthimia_Paraschou.py
@@ -1,9 +1,2 @@
-recipes = [
-    "Georgios_Chazaridis - Kokkinisto me Laxano"
-    "Constantinos_Chamorousios - Makaronia me Kima"
-]
-
-print("Welcome to the Team Cookbook!")
-print("Loaded recipes:")
-for recipe in recipes:
-    print("-", recipe)
+def recipe():
+    return "Pasta and Sauce with meat"
