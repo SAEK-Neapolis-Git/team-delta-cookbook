b9304c3d641423345f491e3c21d9e93217d04883
efipara <efthimiapara06@gmail.com>
2026-01-16 17:55:08 +0200

    Added to recipe_Efthimia_Paraschou.py

---
+++ cookcbook/recipes/recipe_Efthimia_Paraschou.py
@@ -0,0 +1,9 @@
+recipes = [
+    "Georgios_Chazaridis - Kokkinisto me Laxano"
+    "Constantinos_Chamorousios - Makaronia me Kima"
+]
+
+print("Welcome to the Team Cookbook!")
+print("Loaded recipes:")
+for recipe in recipes:
+    print("-", recipe)
