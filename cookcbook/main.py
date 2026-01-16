b0e8592ffc0b4b8ff0572d813448e4be0d9cebd3
efipara <efthimiapara06@gmail.com>
2026-01-16 18:08:05 +0200

    [+] Update to main File by Python

--- cookcbook/main.py
+++ cookcbook/main.py
@@ -1,6 +1,8 @@
 recipes = [
-    "Vasilis Rizos - Porridge with eggs and whey protein powder",
-    "Paraskevh Stamoulh - Pastitsio me tyri"]
+    "Vasilis Rizos - Porridge with eggs and whey protein powder"
+    "Paraskevh Stamoulh - Pastitsio me tyri"
+    "Efthimia_Paraschou - Pasta with Sauce and Meat"
+]
 
 print("Welcome to the Team Cookbook!")
 print("Loaded recipes:")
